8fab8557aaed7c7e827cc4a4674f067daf265ac7
Shantanu Jain <shantanu@openai.com>
2020-09-21 11:44:30 -0700

    test_copying: add test_copytree

--- tests/test_copying.py
+++ tests/test_copying.py
@@ -1,7 +1,11 @@
+import asyncio
+import os
+
 import blobfile
 import pytest
 
 import boostedblob as bbb
+from boostedblob.path import LocalPath
 
 from . import helpers
 
@@ -32,3 +36,33 @@ async def test_copy(any_dir, other_any_dir):
         )
         with blobfile.BlobFile(str(other_any_dir / "copied_small"), "rb") as f:
             assert f.read() == contents_known_small
+
+
+@pytest.mark.asyncio
+@bbb.ensure_session
+async def test_copytree(any_dir, other_any_dir):
+    await asyncio.wait(
+        [
+            helpers.unsafe_create_file(any_dir / "f1"),
+            helpers.unsafe_create_file(any_dir / "f2"),
+            helpers.unsafe_create_file(any_dir / "f3"),
+            helpers.unsafe_create_file(any_dir / "alpha" / "f4"),
+            helpers.unsafe_create_file(any_dir / "alpha" / "f5"),
+            helpers.unsafe_create_file(any_dir / "alpha" / "beta" / "f6"),
+            helpers.unsafe_create_file(any_dir / "alpha" / "beta" / "f7"),
+            helpers.unsafe_create_file(any_dir / "alpha" / "beta" / "gamma" / "f8"),
+            helpers.unsafe_create_file(any_dir / "delta" / "f9"),
+            helpers.unsafe_create_file(any_dir / "delta" / "epsilon" / "f10"),
+        ]
+    )
+
+    async with bbb.BoostExecutor(100) as e:
+        if isinstance(other_any_dir, LocalPath):
+            # TODO: should we need to do this?
+            os.rmdir(other_any_dir)
+        await bbb.copytree(any_dir, other_any_dir, e)
+
+    async def _listtree(d, base):
+        return sorted([p.relative_to(base) async for p in bbb.listtree(d)])
+
+    assert await _listtree(any_dir, any_dir) == await _listtree(other_any_dir, other_any_dir)
