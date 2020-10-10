214c3363694fcea2b75e743ba9452b053e50ef86
Shantanu Jain <shantanu@openai.com>
2020-10-09 22:58:14 -0700

    test.sh: [minor] simplify
    Since mypy's released a new version, we can clean up the hacks I had in
    place earlier. Let's just assume we're always inside poetry's env.

--- test.sh
+++ test.sh
@@ -3,8 +3,7 @@ set -ex
 black --diff --check --quiet .
 isort --diff --check --quiet .
 flake8
-POETRY_PYTHON=$(poetry run which python)
-mypy boostedblob --python-executable $POETRY_PYTHON
-mypy tests --disable-error-code var-annotated --python-executable $POETRY_PYTHON
+mypy boostedblob
+mypy tests --disable-error-code var-annotated
 pytest --cov=boostedblob
 coverage html
