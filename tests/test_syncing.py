de263557f71d52a34997715347b0d0bc72e097b9
Shantanu Jain <shantanu@openai.com>
2021-03-22 23:17:06 -0700

    test_syncing: add single concurrency test

--- tests/test_syncing.py
+++ tests/test_syncing.py
@@ -54,3 +54,31 @@ async def test_sync(any_dir, other_any_dir):
         ]
         await bbb.boost.consume(bbb.sync(any_dir, other_any_dir, e, delete=True))
         assert await _listtree(any_dir, any_dir) == await _listtree(other_any_dir, other_any_dir)
+
+
+@pytest.mark.asyncio
+@bbb.ensure_session
+async def test_sync_single_concurrency():
+    with helpers.tmp_azure_dir() as dir1:
+        with helpers.tmp_azure_dir() as dir2:
+            await asyncio.wait([helpers.unsafe_create_file(dir1 / f"f{i}") for i in range(10)])
+
+        async def _listtree(d, base):
+            return sorted([p.relative_to(base) async for p in bbb.listtree(d)])
+
+        async with bbb.BoostExecutor(1) as e:
+            # sleep since if we run sync too soon, we run into limits of mtime accuracy and end up
+            # syncing more than what we need...
+            await asyncio.sleep(1)
+            await bbb.boost.consume(bbb.sync(dir1, dir2, e))
+            assert await _listtree(dir1, dir1) == await _listtree(dir2, dir2)
+
+            await asyncio.wait(
+                [
+                    cast(Any, bbb.remove(dir1 / "f2")),
+                    helpers.unsafe_create_file(dir1 / "f1", b"sizesame"),
+                    helpers.unsafe_create_file(dir1 / "delta" / "f9", b"differentsize"),
+                ]
+            )
+            assert len([x async for x in bbb.sync(dir1, dir2, e, delete=True)]) == 3
+            assert await _listtree(dir1, dir1) == await _listtree(dir2, dir2)
