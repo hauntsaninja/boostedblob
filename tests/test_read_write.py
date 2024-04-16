fc9c5132d17a646d0755aac2c3d0db05386b9fbe
Jacob Hilton <jacob@alignment.org>
2024-04-16 01:36:43 -0400

    fix test

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -13,7 +13,7 @@ async def test_google_chunking():
     with helpers.tmp_google_dir() as google_dir:
         async with bbb.BoostExecutor(10) as e:
             contents = [b"abc", b"def", b"ghi"]
-            with pytest.raises(ValueError, match="chunked incorrectly"):
+            with pytest.warns(UserWarning, match="chunked incorrectly"):
                 await bbb.write.write_stream(google_dir / "alpha", iter(contents), e)
 
 
