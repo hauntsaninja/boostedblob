hauntsaninja/boostedblob

commit 4594df1a9781e5853ddc11be46dffb009a5c7193

copying: save a network request for cross account copying

boostedblob/copying.py
@@ -83,7 +83,7 @@ async def _cloudpath_copyfile(
         return
     if isinstance(dst, CloudPath):
         if type(src) is type(dst):
-            await cloud_copyfile(src, dst, executor, overwrite=overwrite)
+            await cloud_copyfile(src, dst, executor, overwrite=overwrite, size=size)
             return
 
         if size is not None and size <= config.chunk_size:
@@ -142,7 +142,11 @@ CloudPathT = TypeVar("CloudPathT", bound=CloudPath)
 
 @pathdispatch
 async def cloud_copyfile(
-    src: CloudPathT, dst: CloudPathT, executor: BoostExecutor, overwrite: bool = False
+    src: CloudPathT,
+    dst: CloudPathT,
+    executor: BoostExecutor,
+    overwrite: bool = False,
+    size: Optional[int] = None,
 ) -> None:
     """Copy a file named ``src` to a file named ``dst`` within the same cloud.
 
@@ -153,6 +157,7 @@ async def cloud_copyfile(
     :param dst: The file to copy to.
     :param executor: An executor.
     :param overwrite: If False, raises if the path already exists.
+    :param size: If specified, could save a network call.
 
     """
     raise ValueError(f"Unsupported path: {src}")
@@ -176,7 +181,11 @@ async def _azure_put_block_from_url(
 
 
 async def _azure_cloud_copyfile_via_block_urls(
-    src: AzurePath, dst: AzurePath, executor: BoostExecutor, overwrite: bool = False
+    src: AzurePath,
+    dst: AzurePath,
+    executor: BoostExecutor,
+    overwrite: bool = False,
+    size: Optional[int] = None,
 ) -> None:
     if overwrite:
         await prepare_block_blob_write(dst)
@@ -185,14 +194,15 @@ async def _azure_cloud_copyfile_via_block_urls(
             raise FileExistsError(dst)
 
     copy_source, _ = await azure_auth.generate_signed_url(src)
-    src_size = await getsize(src)
+    if size is None:
+        size = await getsize(src)
 
     # TODO: could consider doing a single Put Block from URL for blobs less than 256MB
 
     byte_ranges = itertools.zip_longest(
-        range(0, src_size, config.chunk_size),
-        range(config.chunk_size, src_size, config.chunk_size),
-        fillvalue=src_size,
+        range(0, size, config.chunk_size),
+        range(config.chunk_size, size, config.chunk_size),
+        fillvalue=size,
     )
 
     upload_id = get_upload_id()
@@ -280,12 +290,18 @@ async def _azure_cloud_copyfile_via_copy(
 
 @cloud_copyfile.register  # type: ignore
 async def _azure_cloud_copyfile(
-    src: AzurePath, dst: AzurePath, executor: BoostExecutor, overwrite: bool = False
+    src: AzurePath,
+    dst: AzurePath,
+    executor: BoostExecutor,
+    overwrite: bool = False,
+    size: Optional[int] = None,
 ) -> None:
     if src.account == dst.account:
         await _azure_cloud_copyfile_via_copy(src, dst, overwrite=overwrite)
     else:
-        await _azure_cloud_copyfile_via_block_urls(src, dst, executor, overwrite=overwrite)
+        await _azure_cloud_copyfile_via_block_urls(
+            src, dst, executor, overwrite=overwrite, size=size
+        )
 
 
 @cloud_copyfile.register  # type: ignore

commit c0ba88f005b37cf96d50aa939862b6afe34d15e7

GCS fixes (#22)
Two small fixes for GCS.

The first fixes a bug introduced in
https://github.com/hauntsaninja/boostedblob/commit/4594df1a9781e5853ddc11be46dffb009a5c7193
where the type signature of `cloud_copyfile` was changed but the GCS
function was not updated, preventing `bbb cptree` between GCS paths from
working.

The second fixes an issue where copying a local file to GCS can fail if
the local file is being written to concurrently.

boostedblob/copying.py
@@ -306,7 +306,11 @@ async def _azure_cloud_copyfile(
 
 @cloud_copyfile.register  # type: ignore
 async def _google_cloud_copyfile(
-    src: GooglePath, dst: GooglePath, executor: BoostExecutor, overwrite: bool = False
+    src: GooglePath,
+    dst: GooglePath,
+    executor: BoostExecutor,
+    overwrite: bool = False,
+    size: Optional[int] = None,
 ) -> None:
     assert isinstance(dst, GooglePath)
     if not overwrite:

boostedblob/write.py
@@ -195,16 +195,22 @@ async def _google_write_stream(
         # chunk. If you upload a chunk with another size you get an HTTP 400 error, unless you tell
         # GCS that it's the last chunk. Since our interface doesn't allow us to know whether or not
         # a given chunk is actually the last chunk, we go ahead and assume that it is if it's an
-        # invalid chunk size. If we receive multiple chunks of invalid chunk size, we throw an
-        # error.
+        # invalid chunk size. If we continue to receive chunks, which can happen if the file is
+        # being written to concurrently, then we discard these additional chunks.
+        if is_finalised:
+            if chunk:
+                import warnings
+
+                warnings.warn(
+                    "The upload was already finalised. A likely cause is a) the file was being "
+                    "written to concurrently, or b) the given stream was chunked incorrectly. "
+                    "Uploads to Google Cloud need to be chunked in multiples of 256 KB "
+                    "(except for the last chunk).",
+                    stacklevel=2,
+                )
+            break
         should_finalise = len(chunk) % (256 * 1024) != 0
         if should_finalise:
-            if is_finalised:
-                raise ValueError(
-                    "The upload was already finalised. A likely cause is the given stream was "
-                    "chunked incorrectly. Uploads to Google Cloud need to be chunked in "
-                    "multiples of 256 KB (except for the last chunk)."
-                )
             total_size = str(end)
             is_finalised = True
 

tests/test_read_write.py
@@ -13,7 +13,7 @@ async def test_google_chunking():
     with helpers.tmp_google_dir() as google_dir:
         async with bbb.BoostExecutor(10) as e:
             contents = [b"abc", b"def", b"ghi"]
-            with pytest.raises(ValueError, match="chunked incorrectly"):
+            with pytest.warns(UserWarning, match="chunked incorrectly"):
                 await bbb.write.write_stream(google_dir / "alpha", iter(contents), e)
 
 